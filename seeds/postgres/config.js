287d0181d32fa5bd7fe8570b51e46467bce92bd6
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-09 23:25:01 -0800

    Connects to pg

---
+++ seeds/postgres/config.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('turash_c', 'postgres', '6042783128', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
