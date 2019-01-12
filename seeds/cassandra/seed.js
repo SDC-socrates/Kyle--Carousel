57f90f4dbd9d887e30359872eb2addfb4a73dc59
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-11 16:47:26 -0800

    Successfully bulk uploads in low volumes

--- seeds/cassandra/seed.js
+++ seeds/cassandra/seed.js
@@ -6,33 +6,39 @@ const client = require('./config.js')
 // SPECIFIC CONFIGS (See seed-helpers for general configs)
 // ========================================================
 
-
-
+const loadBatchSize = 10;
+const totalBatches = 10;
 
 // ========================================================
 // SEED
 // ========================================================
 
-// Use Prepared Statements and Batch Statements
-const query = {
+let queries = [];
+
+const queryTemplate = {
   query: `
     INSERT INTO carsByStatusAndCategory 
       (car_id, status, category, make, model, year, lat, long, photos)
       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     `,
-  params: []
 };
 
-let queries = []
-let params = [12, 'status', 'category', 'make', 'model', 2015, 10.25, -53.1, ['test1', 'test2']];
+// Prepare batched queries with prepared statements per Datastax efficiency recommendations
+let carId = 1;
+for (let batch = 1; batch <= totalBatches; batch++) {
+  // Clear queries
+  queries = [];
+  for (let queryCount = 1; queryCount <= loadBatchSize; queryCount++) {
+    const query = { ...queryTemplate };
+    query.params = [carId, 'status', 'category', 'make', 'model', 2015, 10.25, -53.1, ['test1', 'test2']];
+    queries.push(query);
+    carId += 1;
+  }
+  // Consistency set to 'any' for lowest latency
+  const queryOptions = { prepare: true, consistency: cassandra.types.consistencies.any };
 
-// Consistency set to 'any' for lowest latency
-const queryOptions = { prepare: true, consistency: cassandra.types.consistencies.any };
-
-query.params = params;
-queries.push(query);
-
-client.batch(queries, queryOptions, (err) => {
-  if (err) { console.log(err); }
-  console.log('Data updated on cluster');
-});
+  client.batch(queries, queryOptions, (err) => {
+    if (err) { console.log(err); }
+    console.log('Data updated on cluster');
+  });
+}
