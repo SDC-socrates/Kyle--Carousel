a999432c56d5a49f0dd35bf616d4fb3c14785645
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-11 16:34:43 -0800

    Successfully post one manually written entry to Cassandra

--- seeds/cassandra/seed.js
+++ seeds/cassandra/seed.js
@@ -1,8 +1,38 @@
+const cassandra = require('cassandra-driver');
 const client = require('./config.js')
 
-// query = `
-//   INSERT INTO crossfit_gyms (country_code, state_province, city, gym_name) 
-//     VALUES ('USA', 'CA', 'San Francisco', 'San Francisco CrossFit');
-// `;
-// client.execute(query)
-//   .then(result => console.log(result));
+
+// ========================================================
+// SPECIFIC CONFIGS (See seed-helpers for general configs)
+// ========================================================
+
+
+
+
+// ========================================================
+// SEED
+// ========================================================
+
+// Use Prepared Statements and Batch Statements
+const query = {
+  query: `
+    INSERT INTO carsByStatusAndCategory 
+      (car_id, status, category, make, model, year, lat, long, photos)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `,
+  params: []
+};
+
+let queries = []
+let params = [12, 'status', 'category', 'make', 'model', 2015, 10.25, -53.1, ['test1', 'test2']];
+
+// Consistency set to 'any' for lowest latency
+const queryOptions = { prepare: true, consistency: cassandra.types.consistencies.any };
+
+query.params = params;
+queries.push(query);
+
+client.batch(queries, queryOptions, (err) => {
+  if (err) { console.log(err); }
+  console.log('Data updated on cluster');
+});
