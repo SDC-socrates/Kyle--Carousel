71862083268321d605bc44d8984dcf6eca32ad90
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-14 14:34:04 -0800

    Creates tables but batch sizes too large

--- seeds/cassandra/createTables.js
+++ seeds/cassandra/createTables.js
@@ -2,10 +2,11 @@ const cassandra = require('cassandra-driver');
 const client = require('../../db/cassandra/config.js')
 
 
-// Create 'carsByStatusAndCategory' Table
-// Purpose: Get similar cars given some attributes of an existing car 
-const queryCreateCBSC = `
-  CREATE TABLE IF NOT EXISTS carsByStatusAndCategory (  
+// Create 'cars' Table
+// Purpose: Store and get attributes of a specific car
+// category and status are needed in the primary key as we can only add one additional column in MV later on
+const queryCreateCars = `
+  CREATE TABLE IF NOT EXISTS cars (  
     car_id int,
     status text,
     category text,
@@ -15,36 +16,26 @@ const queryCreateCBSC = `
     lat float,
     long float,
     photos set<text>,
-    PRIMARY KEY (category, status, year, car_id)
-  ) WITH CLUSTERING ORDER BY (status ASC, year DESC, car_id DESC)
+    PRIMARY KEY (car_id, category, status)
+  )
   `;
-const createTable = client.execute(queryCreateCBSC)
+
+const createTable = client.execute(queryCreateCars)
   .then((result) => {
     console.log(result);
-    // Create 'carsByStatusAndCategory' Materialized View
-    // Purpose: Given a car id number, get enough attributes to find full info from the other table
-    const queryCreateCars = `
-      CREATE MATERIALIZED VIEW IF NOT EXISTS cars AS
-        SELECT car_id FROM carsByStatusAndCategory
-        WHERE status IS NOT NULL AND year IS NOT NULL AND car_id IS NOT NULL
-        PRIMARY KEY (car_id, category, status, year)
-        WITH CLUSTERING ORDER BY (car_id DESC)
-      `;
-    client.execute(queryCreateCars)
-      .then(secondResult => console.log(secondResult));
+    // Create 'queryCreateCars' Materialized View
+    // Purpose: Find similar cars. e.g. Select SUVs (compound partition key) that are active (compound partition key) between certain lats (clustering) and years (secondary index), and longs
+    const queryCreateCSCL = `
+    CREATE MATERIALIZED VIEW IF NOT EXISTS carsstatcatlat AS
+      SELECT * FROM cars
+      WHERE category IS NOT NULL AND status IS NOT NULL AND lat IS NOT NULL
+      PRIMARY KEY ((category, status), lat, car_id)
+      WITH CLUSTERING ORDER BY (lat ASC);
+    `;
+    return client.execute(queryCreateCSCL);
+  })
+  .then((secondResult) => {
+    console.log(secondResult);
   });
-
-
-  // categories.name as category, cars.status, cars.long, cars.lat, makes.name as make, models.name as model, models.year, photos.url
-  // Search = Select SUVs (compound partition key) that are active (compound partition key) between certain lats (clustering) and years (secondary index), and longs
-  const queryCreateCars = `
-  CREATE MATERIALIZED VIEW IF NOT EXISTS carsstatcatlat AS
-    SELECT * FROM carsByStatusAndCategory
-    WHERE category IS NOT NULL AND status IS NOT NULL AND lat IS NOT NULL
-    PRIMARY KEY ((category, status), lat)
-    WITH CLUSTERING ORDER BY (lat ASC)
-  `;
-  // We'll also create a secondary index by year.
-
 
 module.exports = createTable;
