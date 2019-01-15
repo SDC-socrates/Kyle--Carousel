1839b20806889613898eb580ff0fbd920b6cc181
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-14 17:08:40 -0800

    Cassandra queries now complete in <50ms

--- seeds/cassandra/createTables.js
+++ seeds/cassandra/createTables.js
@@ -6,7 +6,7 @@ const client = require('../../db/cassandra/config.js')
 // Purpose: Store and get attributes of a specific car
 // category and status are needed in the primary key as we can only add one additional column in MV later on
 const queryCreateCars = `
-  CREATE TABLE IF NOT EXISTS cars (  
+  CREATE TABLE IF NOT EXISTS carsstatcatlat (
     car_id int,
     status text,
     category text,
@@ -16,8 +16,8 @@ const queryCreateCars = `
     lat float,
     long float,
     photos set<text>,
-    PRIMARY KEY (car_id, category, status)
-  )
+    PRIMARY KEY (category, status, lat, car_id)
+  ) WITH CLUSTERING ORDER BY (status DESC);
   `;
 
 const createTable = client.execute(queryCreateCars)
@@ -26,11 +26,11 @@ const createTable = client.execute(queryCreateCars)
     // Create 'queryCreateCars' Materialized View
     // Purpose: Find similar cars. e.g. Select SUVs (compound partition key) that are active (compound partition key) between certain lats (clustering) and years (secondary index), and longs
     const queryCreateCSCL = `
-    CREATE MATERIALIZED VIEW IF NOT EXISTS carsstatcatlat AS
-      SELECT * FROM cars
-      WHERE category IS NOT NULL AND status IS NOT NULL AND lat IS NOT NULL
-      PRIMARY KEY ((category, status), lat, car_id)
-      WITH CLUSTERING ORDER BY (lat ASC);
+    CREATE MATERIALIZED VIEW IF NOT EXISTS cars AS
+      SELECT * FROM carsstatcatlat
+      WHERE category IS NOT NULL AND status IS NOT NULL AND lat IS NOT NULL AND car_id IS NOT NULL
+      PRIMARY KEY (car_id, category, status, lat)
+      WITH CLUSTERING ORDER BY (car_id DESC);
     `;
     return client.execute(queryCreateCSCL);
   })
