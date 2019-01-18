864f10681f1e9b321f2fefa18deab3de59174d6e
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-18 10:53:37 -0800

    Installed new relic

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+require('newrelic');
 const mongodb = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -23,17 +24,21 @@ app.use(bodyParser.json());
 // app.use('/', express.static('client/public'));
 app.listen(port, () => console.log(`Server connected and listening on ${port}!`));
 
+const sendErrOrResults = (res, err, results) => {
+  if (err) {
+    console.log(err);
+    res.status(400).send(JSON.stringify(err));
+  } else {
+    console.log(results);
+    res.send(JSON.stringify(results));
+  }
+};
+
 app.get(/\/api\/cars\/\d+/g, (req, res) => {
   console.log('GET specific car.')
   const carId = req.path.split('/').pop();
   controllers.getSpecificCar(carId, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      console.log(results);
-      res.send(JSON.stringify(results));
-    }
+    sendErrOrResults(res, err, results);
   });
 });
 
@@ -41,12 +46,7 @@ app.post(/\/api\/cars\/\d+/g, (req, res) => {
   console.log('POST specific car:', req.body);
   const carId = req.path.split('/').pop();
   controllers.postSpecificCar(carId, req.body, (err, results) => {
-    if (err) {
-      res.status(400).send(JSON.stringify(err));
-    } else {
-      console.log('FOO2', results);
-      res.send(JSON.stringify(results));
-    }
+    sendErrOrResults(res, err, results);
   });
 });
 
@@ -54,13 +54,7 @@ app.put(/\/api\/cars\/\d+/g, (req, res) => {
   console.log('PUT specific car.')
   const carId = req.path.split('/').pop();
   controllers.putSpecificCar(carId, req.body, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send(JSON.stringify(results));
-    } else {
-      console.log(results);
-      res.send(JSON.stringify(results));
-    }
+    sendErrOrResults(res, err, results);
   });
 });
 
@@ -68,13 +62,7 @@ app.delete(/\/api\/cars\/\d+/g, (req, res) => {
   console.log('DELETE specific car.')
   const carId = req.path.split('/').pop();
   controllers.deleteSpecificCar(carId, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      console.log(results);
-      res.send(JSON.stringify(results));
-    }
+    sendErrOrResults(res, err, results);
   });
 });
 
@@ -85,12 +73,6 @@ app.get('/api/cars', (req, res) => {
   requestedProperties.lat = Number.parseInt(requestedProperties.lat, 10);
   requestedProperties.year = Number.parseInt(requestedProperties.year, 10);
   controllers.getSuggestedCars(requestedProperties, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      console.log(results);
-      res.send(JSON.stringify(results));
-    }
+    sendErrOrResults(res, err, results);
   });
 });
