dd6b69b0e3e9f12fb792a2c71689d435d72c79a0
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-24 15:34:37 -0800

    Merged in some outstanding merge conflicts

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,18 +2,8 @@ const db = require('../../db/postgres/')
 const imageRootURL = 'https://turash-assets.s3.us-west-2.amazonaws.com/';
 
 const getSpecificCar = (carId, callback) => {
-<<<<<<< HEAD
   db.getSpecificCar(carId, (err, results) => {
     // Transform data to client expected shape
-=======
-  // console.timeEnd('Server to controller');
-  // console.time('Controller to DB request');
-  db.getSpecificCar(carId, (err, results) => {
-    // console.timeEnd('DB request to response');
-    // console.time('Transform response to client shape');
-    // Transform data to client expected shape
-    // console.log('DB Results: ', results);
->>>>>>> b8454d780e8878846d51701e98394182abdf55b2
     const output = {
       id: results[0].qid,
       make: `${results[0].qmake} ${results[0].qmodel} ${results[0].qyear}`,
@@ -25,11 +15,6 @@ const getSpecificCar = (carId, callback) => {
       images: [],
     };
     results.forEach(item => output.images.push([`${item.qmake} ${item.qmodel} ${item.qyear}`, imageRootURL + item.qurl]));
-<<<<<<< HEAD
-=======
-    // console.timeEnd('Transform response to client shape');
-    // console.time('Pass client data to respond');
->>>>>>> b8454d780e8878846d51701e98394182abdf55b2
     callback(err, output);
   });
 };
@@ -79,18 +64,11 @@ const getSuggestedCars = (requestedProperties, callback) => {
       suggestedCars[car.make + car.model + car.year + car.long + car.lat] = {
         id: car.id,
         make: `${car.make} ${car.model} ${car.year}`,
-<<<<<<< HEAD
         long: car.long,
         lat: car.lat,
         thumb: imageRootURL + car.url
       };
     }
-=======
-        thumb: imageRootURL + car.url
-      };
-    }
-//    console.log('RESULTS', Object.values(suggestedCars));
->>>>>>> b8454d780e8878846d51701e98394182abdf55b2
     callback(err, Object.values(suggestedCars));
   });
 };
