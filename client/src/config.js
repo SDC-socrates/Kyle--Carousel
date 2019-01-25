ab042909cedeac15111eb0c24a8f48e32a197fe1
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-25 14:56:28 -0800

    Additional address error

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // Comment out the relevant line depending on whether you wish to connect to a local or remote API
 
 // const apiRootUrl = 'http://localhost:3004';
-const apiRootUrl = 'http://http://turashc-lb-1894563298.us-west-2.elb.amazonaws.com:3004';
+const apiRootUrl = 'http://turashc-lb-1894563298.us-west-2.elb.amazonaws.com:3004';
 
 export default apiRootUrl;
